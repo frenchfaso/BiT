b6a2ed1ee81949409f42cfa3ab6fffd875e0f0d4
frenchfaso <frenchfaso@gmail.com>
2019-09-06 17:02:43 +0200

    added A* pathfinding

--- src/modules/map.js
+++ src/modules/map.js
@@ -1,5 +1,7 @@
 class Map {
     constructor() {
+        this.res = 3;
+        this.path = [];
         this.data = [
             [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 4, 4, 6, 4, 4, 6, 4, 6, 4, 4, 4, 6, 4],
             [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
@@ -29,11 +31,20 @@ class Map {
     }
 
     getMap(w, h) {
-        this.createMaze(w, h);
-        for(let d of this.data){
+        let maze = this.createMaze(w, h);
+        for (let d of maze) {
             console.log(...d);
         }
-        return this.data;
+        this.path = this.aStar(maze, {x:1, y:1}, {x:w-3, y:h-3});
+        // let m = [...maze];
+        // for(let n of this.path){
+        //     m[n.x][n.y] = 8;
+        // }
+        // console.log("So..you like it the easy way.. don't you?");
+        // for(let k of m){
+        //     console.log(...k);
+        // }
+        return maze;
     }
     createMaze(w, h) {
         let maze = [];
@@ -52,7 +63,7 @@ class Map {
             a.shift();
         }
         maze.shift();
-        this.data = maze;
+        return maze;
     }
 
     createVerticalWall(maze, x1, y1, x2, y2) {
@@ -67,8 +78,8 @@ class Map {
                 maze[midX][i] = 1;
             }
         }
-        if (midX - x1 > 3) this.createHorizontalWall(maze, x1, y1, midX, y2);
-        if (x2 - midX > 3) this.createHorizontalWall(maze, midX, y1, x2, y2);
+        if (midX - x1 > this.res) this.createHorizontalWall(maze, x1, y1, midX, y2);
+        if (x2 - midX > this.res) this.createHorizontalWall(maze, midX, y1, x2, y2);
     }
 
     createHorizontalWall(maze, x1, y1, x2, y2) {
@@ -83,8 +94,98 @@ class Map {
                 maze[i][midY] = 1;
             }
         }
-        if (midY - y1 > 3) this.createVerticalWall(maze, x1, y1, x2, midY);
-        if (y2 - midY > 3) this.createVerticalWall(maze, x1, midY, x2, y2);
+        if (midY - y1 > this.res) this.createVerticalWall(maze, x1, y1, x2, midY);
+        if (y2 - midY > this.res) this.createVerticalWall(maze, x1, midY, x2, y2);
+    }
+
+    aStar(maze, start, end) {
+        Array.prototype.remove = function (item) {
+            for (let i = this.length - 1; i >= 0; i--) {
+                if (this[i] === item) {
+                    this.splice(i, 1);
+                }
+            }
+        }
+
+        let openSet = [];
+        let closedSet = [];
+        let nodes = [];
+        for (let x = 0; x < maze.length; x++) {
+            for (let y = 0; y < maze[0].length; y++) {
+                let n = {
+                    x: x,
+                    y: y,
+                    wall: maze[x][y] != 0 ? true : false,
+                    neighbours: [],
+                    f: 0,
+                    g: 0,
+                    h: Math.sqrt(Math.pow(end.x - x, 2) + Math.pow(end.y - y, 2)),
+                    start: (x == start.x && y == start.y) ? true : false,
+                    end: (x == end.x && y == end.y) ? true : false
+                };
+                nodes.push(n);
+            }
+        }
+        for (let n of nodes) {
+            let left = n.x - 1;
+            let right = n.x + 1;
+            let up = n.y - 1;
+            let down = n.y + 1;
+            if (left >= 0) n.neighbours.push(nodes.find(e => (e.x == left && e.y == n.y)));
+            if (right < maze.length - 1) n.neighbours.push(nodes.find(e => (e.x == right && e.y == n.y)));
+            if (up >= 0) n.neighbours.push(nodes.find(e => (e.y == up && e.x == n.x)));
+            if (down < maze[0].length - 1) n.neighbours.push(nodes.find(e => (e.y == down && e.x == n.x)));
+        }
+        openSet.push(nodes.find(e => e.start == true));
+
+        while (openSet.length > 0) {
+            let winner = 0;
+            for (let i = 0; i < openSet.length; i++) {
+                if (openSet[i].f < openSet[winner].f) {
+                    winner = i;
+                }
+            }
+
+            let current = openSet[winner];
+
+            if (current.end == true) {
+                // get path
+                let p = [];
+                let temp = current;
+                p.push(temp);
+                while (temp.previous) {
+                    p.push(temp.previous);
+                    temp = temp.previous;
+                }
+                return p;
+            }
+
+            openSet.remove(current);
+            closedSet.push(current);
+
+            for (let n of current.neighbours) {
+                if (!closedSet.find(e => e === n) && n.wall == false) {
+
+                    let tempG = current.g + 1;
+
+                    let newPath = false;
+                    if (openSet.find(e => e === n)) {
+                        if (tempG < n.g) {
+                            n.g = tempG;
+                            newPath = true;
+                        }
+                    } else {
+                        n.g = tempG;
+                        newPath = true;
+                        openSet.push(n);
+                    }
+                    if (newPath == true) {
+                        n.f = n.g + n.h;
+                        n.previous = current;
+                    }
+                }
+            }
+        }
     }
 }
 
